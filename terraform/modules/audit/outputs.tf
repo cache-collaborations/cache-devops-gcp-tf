7c9d953944d06079354d9085bfb03b924b37e8f6
e-ian <ianemma70@gmail.com>
2025-04-24 21:45:33 +0300

    update audit module outputs

--- terraform/modules/audit/outputs.tf
+++ terraform/modules/audit/outputs.tf
@@ -1,8 +1,10 @@
 # Outputs
 output "audit_logs_bucket" {
-  value = google_storage_bucket.audit_logs.name
+  value       = google_logging_project_bucket_config.audit_logs.bucket_id
+  description = "Audit logs bucket name"
 }
 
-output "audit_logs_sink" {
-  value = google_logging_project_sink.audit_logs_sink.name
+output "soc2_dashboard" {
+  value       = google_monitoring_dashboard.soc2_dashboard.dashboard_json
+  description = "SOC 2 compliance dashboard JSON"
 }
