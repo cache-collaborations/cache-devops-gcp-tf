b78a3f3b02b6d7f59dcd4ccee695822b6151e2b3
e-ian <ianemma70@gmail.com>
2025-04-24 21:39:58 +0300

    update terraform iam module outputs

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,12 +1,20 @@
-# Output service account emails
-output "gke_service_account_email" {
-  value = google_service_account.gke_service_account.email
+# Outputs
+output "gke_sa_email" {
+  value       = google_service_account.gke_sa.email
+  description = "Email of the GKE service account"
 }
 
-output "app_service_account_email" {
-  value = google_service_account.app_service_account.email
+output "app_sa_email" {
+  value       = google_service_account.app_sa.email
+  description = "Email of the application service account"
 }
 
-output "monitoring_service_account_email" {
-  value = google_service_account.monitoring_service_account.email
+output "db_sa_email" {
+  value       = google_service_account.db_sa.email
+  description = "Email of the database service account"
+}
+
+output "audit_sa_email" {
+  value       = google_service_account.audit_sa.email
+  description = "Email of the audit service account"
 }
